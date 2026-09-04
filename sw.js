e818fb714a8f17f8006396cb1db2c1e63c4235f8
yirouchien <yirouchien@gmail.com>
2026-09-04 13:41:04 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE='jp-expense-v1';
+const ASSETS=['./','./index.html','./manifest.webmanifest'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
