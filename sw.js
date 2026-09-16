51e801351899c0e4a3855832058e58cea760db20
yirouchien <yirouchien@gmail.com>
2026-09-16 11:40:07 +0800

    Update cache version from v1 to v3

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='jp-expense-v1';
+const CACHE='jp-expense-v3';
 const ASSETS=['./','./index.html','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
